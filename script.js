87fc1f6de8ddd1fde0ba48361441d145e16f5221
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-01-07 12:01:11 +0530

    code added

--- script.js
+++ script.js
@@ -6,22 +6,71 @@ let arr = [
   { id: 3, name: "karen", age: "19", profession: "admin" },
 ];
 
+//----------------------------------Map Function------------------------------
 function PrintDeveloperswithMap() {
   //Write your code here
+  arr.map(printingThroghMap); // Each element is stored in "printingThroghMap" variable
+
+  function printingThroghMap(arrayItem) {
+    if (arrayItem.profession === "developer") {
+      console.log(arrayItem);
+    }
+  }
 }
 
+//----------------------------------For Each Function------------------------------
 function PrintDeveloperbyForEach() {
   //Write your code here
+  arr.forEach(printingThroghForEach); // Each element is stored in "printingThroghForEach" variable
+
+  function printingThroghForEach(arrayItem) {
+    if (arrayItem.profession === "developer") {
+      console.log(arrayItem);
+    }
+  }
 }
 
+//----------------------------------Add Data Function------------------------------
 function addData() {
   //Write your code here
+  let newObj = { id: 4, name: "susan", age: "20", profession: "intern" };
+  arr.push(newObj);
+  console.log(arr);
 }
 
+//----------------------------------Remove Admin Function------------------------------
 function removeAdmin() {
   //Write your code here
+  let filtered_arr = arr.filter(function (val) {
+    //callback function
+    if (val.profession !== "admin") {
+      //filtering criteria
+      return val;
+    }
+  });
+  console.log(filtered_arr);
 }
 
+//----------------------------------Concatinate Function------------------------------
 function concatenateArray() {
   //Write your code here
+  let arr2 = [
+    { id: 5, name: "swapnilC", age: "30", profession: "PROgrammer" },
+    { id: 6, name: "akshayG", age: "28", profession: "developer" },
+    { id: 7, name: "vidhiJ", age: "21", profession: "Manager" },
+  ];
+
+  let concatArray = arr.concat(arr2);
+  console.log(concatArray);
 }
+
+// Terminal output
+// PrintDeveloperswithMap(arr);
+// console.log();
+// PrintDeveloperbyForEach(arr);
+// console.log();
+// addData(arr);
+// console.log();
+// removeAdmin(arr);
+// console.log();
+// concatenateArray(arr);
